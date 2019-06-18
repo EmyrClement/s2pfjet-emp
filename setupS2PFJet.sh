b78edc7fb38c90b417d98385657b9624dd300e03
Aaron Bundock <aaron.bundock@cern.ch>
2019-06-18 23:56:01 +0200

    Update setup script

--- setupS2PFJet.sh
+++ setupS2PFJet.sh
@@ -7,19 +7,19 @@ source ~/ipbb-0.4.2/env.sh
 ipbb init s2PFJet
 cd s2PFJet
 
-runCmd 'ipbb add git https://:@gitlab.cern.ch:8443/p2-xware/firmware/emp-fwk.git -b v0.2.3'
-runCmd 'ipbb add git https://:@gitlab.cern.ch:8443/cms-cactus/firmware/mp7.git -b ephemeral/phase2-vC'
-runCmd 'ipbb add git https://github.com/ipbus/ipbus-firmware -b v1.3'
+ipbb add git https://:@gitlab.cern.ch:8443/p2-xware/firmware/emp-fwk.git -b v0.2.3
+ipbb add git https://:@gitlab.cern.ch:8443/cms-cactus/firmware/mp7.git -b ephemeral/phase2-vC
+ipbb add git https://github.com/ipbus/ipbus-firmware -b v1.3
 
 
-# Checkout HLS algo and synthesize
+# Checkout HLS algo and emp-fwk and synthesize
 runCmd 'ipbb add git https://gitlab.cern.ch/sbologna/fpga-clustering.git -b line_data'
+runCmd 'ipbb add git https://github.com/bundocka/s2pfjet-emp.git'
 cd src/fpga-clustering
 runCmd 'vivado_hls FPGAClustering_ExportIP_KU115.tcl'
 cd ../../
 
-# Checkout emp-fwk vivado project
-runCmd 'ipbb add git https://gitlab.cern.ch/bundocka/s2pfjet-emp.git'
+# Create vivado project
 runCmd 'ipbb proj create vivado s2PFJetClust s2pfjet-emp:projects/ku115dc_s2pfjet  -t top.dep'
 cd proj/s2PFJetClust
 
