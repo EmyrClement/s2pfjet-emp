ed06d32b481436f4afd88ccba420031ca1d17495
Aaron Bundock <aaron.bundock@cern.ch>
2019-06-29 01:21:25 +0200

    Update path

--- projects/includeS2PFJet.tcl
+++ projects/includeS2PFJet.tcl
@@ -11,8 +11,7 @@ file mkdir $ipRepoDir
 set_property  ip_repo_paths  $ipRepoDir [current_project]
 # Rebuild user ip_repo's index before adding any source files
 update_ip_catalog -rebuild
-update_ip_catalog -add_ip "/home/bundocka/fullChain/cern-cms_jet_trigger_chain_wrapper_1_0.zip" -repo_path $ipRepoDir
-
+update_ip_catalog -add_ip "/home/bundocka/fullChain2/s2PFJet/src/phase-2-jet-trigger-chain/IPs/jet_trigger_chain/cern-cms_hls_jet_trigger_chain_wrapper_1.0.zip" -repo_path $ipRepoDir
 create_ip -name ${algo} -vendor cern-cms -library hls -version ${version} -module_name ${modName}
 generate_target {instantiation_template} [get_files top/top.srcs/sources_1/ip/${modName}/${modName}.xci]
 generate_target all [get_files top/top.srcs/sources_1/ip/${modName}/${modName}.xci]
