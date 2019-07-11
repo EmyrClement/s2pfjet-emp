51e58b9d2b0bd43915750e36a85a2e98627bea28
Aaron Bundock <aaron.bundock@cern.ch>
2019-07-11 17:42:36 +0200

    Update script

--- setupS2PFJet.sh
+++ setupS2PFJet.sh
@@ -10,12 +10,12 @@ ipbb add git https://:@gitlab.cern.ch:8443/cms-cactus/firmware/mp7.git -b epheme
 ipbb add git https://github.com/ipbus/ipbus-firmware -b v1.3
 ipbb add git https://:@gitlab.cern.ch:8443/sbologna/phase-2-jet-trigger-chain.git -b manual_pack_unpack
 ipbb add git https://github.com/bundocka/s2pfjet-emp.git -b fullChainNew
-runCmd 'cd src/phase-2-jet-trigger-chain/'
+cd src/phase-2-jet-trigger-chain/
 vivado_hls Phase2JetTrigger_ExportIP.tcl
 source GetIPs.sh
 vivado -mode batch -source jet_trigger_chain.tcl
 cd ../../
-ipbb proj create vivado s2PFJetClust s2pfjet-emp:projects/ku115dc_s2pfjet  -t top.dep
+runCmd 'ipbb proj create vivado s2PFJetClust s2pfjet-emp:projects/ku115dc_s2pfjet  -t top.dep'
 cd proj/s2PFJetClust
 ipbb vivado project
 ipbb vivado synth -j4 impl -j4
