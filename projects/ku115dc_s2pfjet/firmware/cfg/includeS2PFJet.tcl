17b32af38141312edc550de8506e2e288d4f0096
Aaron Bundock <aaron.bundock@cern.ch>
2019-10-07 19:40:27 +0200

    top->s2PFJetClust for new ipbb v0.5.2

--- projects/ku115dc_s2pfjet/firmware/cfg/includeS2PFJet.tcl
+++ projects/ku115dc_s2pfjet/firmware/cfg/includeS2PFJet.tcl
@@ -11,8 +11,8 @@ set_property  ip_repo_paths  $ipRepoDir [current_project]
 update_ip_catalog -rebuild
 update_ip_catalog -add_ip "$dir/phase-2-jet-trigger-chain/IPs/jet_trigger_chain/cern-cms_hls_jet_trigger_chain_wrapper_1.0.zip" -repo_path $ipRepoDir
 create_ip -name ${algo} -vendor cern-cms -library hls -version ${version} -module_name ${modName}
-generate_target {instantiation_template} [get_files top/top.srcs/sources_1/ip/${modName}/${modName}.xci]
-generate_target all [get_files top/top.srcs/sources_1/ip/${modName}/${modName}.xci]
-export_ip_user_files -of_objects [get_files top/top.srcs/sources_1/ip/${modName}/${modName}.xci] -no_script -force -quiet
-create_ip_run [get_files -of_objects [get_fileset sources_1] top/top.srcs/sources_1/ip/${modName}/${modName}.xci]
+generate_target {instantiation_template} [get_files s2PFJetClust/s2PFJetClust.srcs/sources_1/ip/${modName}/${modName}.xci]
+generate_target all [get_files s2PFJetClust/s2PFJetClust.srcs/sources_1/ip/${modName}/${modName}.xci]
+export_ip_user_files -of_objects [get_files s2PFJetClust/s2PFJetClust.srcs/sources_1/ip/${modName}/${modName}.xci] -no_script -force -quiet
+create_ip_run [get_files -of_objects [get_fileset sources_1] s2PFJetClust/s2PFJetClust.srcs/sources_1/ip/${modName}/${modName}.xci]
 launch_run -jobs 8 ${modName}_synth_1
--- setupS2PFJet.sh
+++ setupS2PFJet.sh
@@ -6,9 +6,8 @@ source ~/ipbb-0.5.2/env.sh
 ipbb init s2PFJet
 cd s2PFJet
 ipbb add git https://:@gitlab.cern.ch:8443/p2-xware/firmware/emp-fwk.git -b v0.3.0
-ipbb add git https://gitlab.cern.ch/ttc/legacy_ttc.git -b v2.1
 ipbb add git https://github.com/ipbus/ipbus-firmware -b v1.5
-ipbb add git https://:@gitlab.cern.ch:8443/cms-cactus/firmware/mp7.git -b ephemeral/phase2-vC
+ipbb add git https://gitlab.cern.ch/ttc/legacy_ttc.git -b v2.1
 ipbb add git https://:@gitlab.cern.ch:8443/sbologna/phase-2-jet-trigger-chain.git -b 0.10
 ipbb add git https://github.com/bundocka/s2pfjet-emp.git
 cd src/phase-2-jet-trigger-chain/
